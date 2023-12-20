38cf4e65b9758087395eb18430dcbea461a59270
rinat <qefeq@ru>
2023-12-21 00:39:39 +0500

    final

---
+++ app.js
@@ -0,0 +1,28 @@
+
+var imya = prompt ('Ведите свое имя')
+
+console.log( "меня зовут " + imya );
+
+var age = prompt ('Ведите свой возраст')
+
+console.log("Мне " + age );
+
+var nan = prompt ("Решите примет 2 + 2 = ?")
+
+console.log("Правельный ответ 4: " + 'Ваш ответ ' + nan  );
+
+var nan2 = prompt ("Решите примет 15 - 5 = ?")
+
+console.log("Правельный ответ 10: " + 'Ваш ответ ' + nan2  );
+
+var nan3 = prompt ("Решите примет 5 * 5 = ?")
+
+console.log("Правельный ответ 25: " + 'Ваш ответ ' + nan3  );
+
+var nan4 = prompt ("Решите примет 10 / 2 = ?")
+
+console.log("Правельный ответ 5: " + 'Ваш ответ ' + nan4  );
+
+var nan5 = prompt ("Решите примет 10 % 2 = ?")
+
+console.log("Правельный ответ 0.1: " + 'Ваш ответ ' + nan5  );
